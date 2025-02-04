4a6cef591e00b797ea61df83002a66b669625f5e
arunkumartaneja <taneja.arun1@gmail.com>
2025-02-04 14:29:17 +0530

    fixed inventory bug

--- inventory/src/main/java/com/shopping/inventory/model/Inventory.java
+++ inventory/src/main/java/com/shopping/inventory/model/Inventory.java
@@ -3,11 +3,15 @@ package com.shopping.inventory.model;
 import java.io.Serializable;
 import java.util.List;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Inventory implements Serializable {
 
 	private static final long serialVersionUID = 3186836634025440782L;
--- inventory/src/main/java/com/shopping/inventory/model/Product.java
+++ inventory/src/main/java/com/shopping/inventory/model/Product.java
@@ -2,11 +2,15 @@ package com.shopping.inventory.model;
 
 import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class Product implements Serializable {
 
 	private static final long serialVersionUID = 1500333661782910889L;
